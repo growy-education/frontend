c19fe4fa727e55a60b17a3c6415879d15e558782
HirokiTakekuma <takekumahiroki@gmail.com>
2023-10-18 19:30:50 +0900

    Coaching capasity is fullgit status

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -45,7 +45,7 @@ export const LandingPage = () => {
             severity="info"
             sx={{ width: "100%" }}
           >
-            現在コーチングの定員が残りわずかとなっております。
+            現在コーチングの定員が満員となっております。
           </Alert>
         </Snackbar>
 
